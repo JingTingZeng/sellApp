9d55d91ce7891a6b0bee46943dfe8b847b9a6164
JingTing <kelly20829tw@outlook.com>
2020-01-16 14:50:52 +0800

    新增“商家詳情”彈跳視窗

--- src/views/layout/header.vue
+++ src/views/layout/header.vue
@@ -16,27 +16,75 @@
                     <span v-bind:class="classMap[seller.supports[0].type]" class="support-icon"></span>
                     <span class="support-text">{{seller.supports[0].description}}</span>
                 </div>
-                <div v-if="seller.supports" class="support-count">
+                <div v-if="seller.supports" v-on:click="toggleDetail" class="support-count">
                     <span class="count">{{seller.supports.length}}個</span>
                     <i class="icon-keyboard_arrow_right"></i>
                 </div>
             </div>
         </div>
-        <div class="bulletin">
+        <div v-on:click="toggleDetail" class="bulletin">
             <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
             <i class="icon-keyboard_arrow_right"></i>
         </div>
         <div class="background">
             <img width="100%" height="100%" v-bind:src="seller.avator" alt="">
         </div>
+        <transition name="fade">
+            <div v-show="detailShow" class="detail">
+                <div class="detail-content clearfix">
+                    <div class="detail-body">
+                        <h1 class="detail-name">{{seller.name}}</h1>
+                        <div class="detail-star-content">
+                            <star v-bind:size="48" v-bind:score="seller.score"/>
+                        </div>
+                        <div class="detail-title">
+                            <div class="line"></div>
+                            <div class="text">優惠訊息</div>
+                            <div class="line"></div>
+                        </div>
+                        <ul v-if="seller.supports" class="supports">
+                            <li v-for="(item,index) in seller.supports" v-bind:key="index" class="support-item">
+                                <span v-bind:class="classMap[item.type]" class="support-icon"></span>
+                                <span class="support-text">{{item.description}}</span>
+                            </li>
+                        </ul>
+                        <div class="detail-title">
+                            <div class="line"></div>
+                            <div class="text">商家公告</div>
+                            <div class="line"></div>
+                        </div>
+                        <div class="detail-bulletin">
+                            <p class="detail-bulletin-content">{{seller.bulletin}}</p>
+                        </div>
+                    </div>
+                </div>
+                <div v-on:click="toggleDetail" class="detail-close">
+                    <i class="icon-close"></i>
+                </div>
+            </div>
+        </transition>
     </div>
 </template>
 
 <script>
+import star from '@/components/star';
 export default {
+    components:{
+        star
+    },
     props:{
         seller:{
             type:Object
+        }
+    },
+    data(){
+        return{
+            detailShow:false
+        }
+    },
+    methods:{
+        toggleDetail(){
+            this.detailShow = !this.detailShow; 
         }
     },
     created(){
