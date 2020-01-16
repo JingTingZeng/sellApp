338b3485b1dcc9332d6e7af3a07cdfd78b195117
JingTing <kelly20829tw@outlook.com>
2020-01-16 14:50:06 +0800

    新增星星組件

---
+++ src/components/star.vue
@@ -0,0 +1,45 @@
+<template>
+    <div v-bind:class="starType" class="star">
+        <span v-for="(item,index) in itemClasses" v-bind:key="index" v-bind:class="item" class="star-item"></span>
+    </div>
+</template>
+
+<script>
+const LENGTH = 5;
+const CLS_ON = 'on';
+const CLS_HALF = 'half';
+const CLS_OFF = 'off';
+
+export default {
+    props:{
+        size:{
+            type: Number
+        },
+        score:{
+            type: Number
+        }
+    },
+    computed:{
+        starType(){
+            return 'star-'+this.size
+        },
+        itemClasses(){
+            let result = [];
+            let score = Math.floor(this.score*2) /2;
+            let hasDecimal = score % 1 !==0;
+            let integer = Math.floor(score);
+
+            for(let i =0 ; i <integer ; i++){
+                result.push(CLS_ON);
+            }
+            if(hasDecimal){
+                result.push(CLS_HALF);
+            }
+            while(result.length < LENGTH){
+                result.push(CLS_OFF);
+            }
+            return result;
+        }
+    }
+}
+</script>
