a36df7d249a161cda50d6566ff3e5cdf2266e341
jingtingzeng <kelly20829tw@gmail.com>
2020-02-02 22:02:24 +0800

    新增購物車清單

--- src/views/shopCart/shopcart.vue
+++ src/views/shopCart/shopcart.vue
@@ -1,6 +1,7 @@
 <template>
+<div>
     <div class="shopcart">
-        <div class="shopcart-content">
+        <div v-on:click="toggleList" class="shopcart-content">
             <div class="shopcart-left">
                 <div class="shopcart-icon-content">
                     <div  v-bind:class="{'hightlight':totalCount>0}" class="shopcart-icon">
@@ -11,7 +12,7 @@
                 <div v-bind:class="{'hightlight':totalPrice>0}" class="shopcart-amount-price">${{totalPrice}}</div>
                 <div class="shopcart-desc">另需運費${{deliveryPrice}}</div>
             </div>
-            <div class="shopcart-right">
+            <div v-on:click.stop.prevent="pay" class="shopcart-right">
                 <div v-bind:class="payClass" class="pay">
                     {{payDesc}}
                 </div>
@@ -24,11 +25,41 @@
                 </div>
             </transition-group>
         </div>
+        <transition name="fold">
+            <div v-show="listShow" class="shopcart-list">
+                <div class="shopcart-list-header">
+                    <div class="shopcart-title">購物車</div>
+                    <div v-on:click="empty" class="empty">清空</div>
+                </div>
+                <div ref="shopcartListWrapper" class="shopcart-list-body">
+                    <ul>
+                        <li v-for="item in selectProducts" class="shopcart-list-item">
+                            <span class="name">{{item.name}}</span>
+                            <div class="price">
+                                <span>NT${{item.price*item.count}}</span>
+                            </div>
+                            <div class="cartControl-content">
+                                <cartControl v-bind:product="item" />
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </transition>
     </div>
+    <transition name="fade">
+        <div v-show="listShow" v-on:click="hideList" class="list-mask"></div>
+    </transition>
+</div>
 </template>
 
 <script>
+import BScroll from 'better-scroll';
+import cartControl from '@/components/cartControl';
 export default {
+    components:{
+        cartControl
+    },
     props:{
         selectProducts:{
             type:Array,
@@ -64,7 +95,8 @@ export default {
                 //     show: false
                 // }
             ],
-            dropBalls:[]
+            dropBalls:[],
+            fold: true
         }
     },
     computed: {
@@ -97,9 +129,47 @@ export default {
                 return 'not-enough'
             }
             return 'enough'
+        },
+        listShow(){
+            if(!this.totalCount){
+                this.fold = true;
+                return false;
+            }
+            let show = !this.fold;
+            if(show){
+                if(!this.scroll){
+                    this.scroll = new BScroll(this.$refs['shopcartListWrapper'],{
+                        click:true
+                    });
+                }else{
+                    this.scroll.refresh();
+                }
+                
+            }
+            return show;
         }
     },
     methods:{
+        pay(){
+            if(this.totalPrice < this.minPrice){
+                return;
+            }
+            window.alert(`總金額${this.totalPrice}元`);
+        },
+        toggleList(){
+            if(!this.totalCount){
+                return;
+            }
+            this.fold = !this.fold;
+        },
+        hideList(){
+            this.fold = true;
+        },
+        empty(){
+            this.selectProducts.forEach((item)=>{
+                item.count = 0;
+            })
+        },
         drop(element){
             for(let i=0;i<this.balls.length;i++){
                 let ball = this.balls[i];
